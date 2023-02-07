e39e067aabaca33e624d3ddc67c061244ad49f31
niklas <grieger@fh-aachen.de>
2023-02-07 14:35:40 +0100

    add configurable preprocessing script + fix resampling in preprocessing + add robustsleepnet models with their own preprocessing

--- preprocessing_rsn.py
+++ preprocessing_rsn.py
@@ -1,7 +1,15 @@
 import numpy as np
 from scipy.signal import butter as _butter
-from scipy.signal import sosfiltfilt as _sosfiltfilt, resample_poly
+from scipy.signal import sosfiltfilt as _sosfiltfilt
+from scipy.signal import stft, resample_poly
 from sklearn.preprocessing import RobustScaler
+
+
+def standardize_online(x, eps=1e-15, axis=(0, -1)):
+    # temporal_context, n_channels, f_fft, l_fft = x.shape
+    mu = np.mean(x, axis=axis, keepdims=True)
+    sigma = np.std(x, axis=axis, keepdims=True, ddof=1)
+    return (x - mu) / (sigma + eps)
 
 
 def preprocess(channel_data, sampling_rates, preprocessing_sampling_rate):
@@ -12,7 +20,8 @@ def preprocess(channel_data, sampling_rates, preprocessing_sampling_rate):
 
     data_new = np.zeros((n_channels, preprocessing_sampling_rate * 30 * int(data.shape[1] / sampling_rate / 30)))
     for i in range(n_channels):
-        bandpass_freqs = 2 * np.array([0.3, 35]) / sampling_rate
+        # bandpass_freqs = 2 * np.array([0.3, 35]) / sampling_rate
+        bandpass_freqs = 2 * np.array([0.2, 30]) / sampling_rate
         sos = _butter(4, bandpass_freqs, btype='band', output='sos')
         x = _sosfiltfilt(sos, data[i], axis=-1, padtype='constant')
 
@@ -50,9 +59,31 @@ def preprocess(channel_data, sampling_rates, preprocessing_sampling_rate):
             idx_end = scaled_data.shape[0]
         x = scaled_data[idx_start:idx_end]
         x = np.concatenate([left_epochs, x, right_epochs], axis=0, dtype='float32')
-        def_samples.append(x.flatten())
+        # sequence_length, channels, data_points
 
-    return [11, n_channels, preprocessing_sampling_rate * 30], def_samples
+        # convert to stft
+        window_length = 2
+        window_stride = 1
+        n_fft = 2 ** int(np.log2(preprocessing_sampling_rate * window_length) + 1)
+        n_stride = int(preprocessing_sampling_rate * window_stride)
+        window = np.hanning
+        window = window(n_fft) / window(n_fft).sum()
+        # _, _, x_stft = stft(x, preprocessing_sampling_rate, nperseg=n_fft, noverlap=n_fft - n_stride, nfft=n_fft,
+        #                     scaling='spectrum', return_onesided=True, padded=False, window=window, boundary=None)
+        _, _, x_stft = stft(x, preprocessing_sampling_rate, nperseg=n_fft, noverlap=n_fft - n_stride, nfft=n_fft,
+                            return_onesided=True, padded=False, window=window, boundary=None)
+        x_stft = x_stft.squeeze()
+        # sequence_length, channels, f_fft, l_fft
+        x_stft = (x_stft ** 2)
+        x_stft = np.abs(x_stft)
+        x_stft = np.clip(x_stft, 1e-20, 1e32)
+        x_stft = np.log10(x_stft)
+        x_stft = standardize_online(x_stft)
+        f_fft, l_fft = x_stft.shape[-2:]
+
+        def_samples.append(x_stft.flatten())
+
+    return [11, n_channels, f_fft, l_fft], def_samples
 
 
 # params are set in global context by webworker
